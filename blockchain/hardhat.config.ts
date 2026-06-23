d451d08e44d4f305e88ca64ea6cbf5b1c5dbfd60
smarth khurana <khuranasmarth06@gmail.com>
2026-06-23 22:49:02 +0530

    initialise hardhat project

---
+++ blockchain/hardhat.config.ts
@@ -0,0 +1,44 @@
+import type { HardhatUserConfig } from "hardhat/config";
+import hardhatEthers from "@nomicfoundation/hardhat-ethers";
+import hardhatVerify from "@nomicfoundation/hardhat-verify";
+import hardhatToolboxMochaEthers from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import hardhatNetworkHelpers from "@nomicfoundation/hardhat-network-helpers";
+import * as dotenv from "dotenv";
+dotenv.config();
+const config: HardhatUserConfig = {
+  plugins: [
+    hardhatEthers,
+    hardhatVerify,
+    hardhatToolboxMochaEthers,
+    hardhatNetworkHelpers,
+    
+  ],
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+
+  networks: {
+    sepolia: {
+      type: "http",
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY
+        ? [process.env.PRIVATE_KEY]
+        : [],
+    },
+  },
+
+  verify: {
+    etherscan: {
+      apiKey: process.env.ETHERSCAN_API_KEY,
+    },
+  },
+};
+
+export default config;
+
