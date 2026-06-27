d7d28937903f00c09f0c271f33c1c9e2dad8f657
smarth khurana <khuranasmarth06@gmail.com>
2026-06-27 20:20:20 +0530

    better home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,32 +6,53 @@ function Home() {
     <>
       <Navbar />
       <div className="home">
-        <section className="hero">
-          <div className="hero-left">
-            <h1>
-              Blockchain Based Academic Certificate Verifier 
-            </h1>
-            <p>
-              Securely issue, manage and verify
-              certificates using blockchain
-              technology.
-            </p>
-            <div className="hero-buttons">
-              <button className="primary-btn">
-                Verify Certificate
-              </button>
-              <button className="secondary-btn">
-                Institute Portal
-              </button>
-            </div>
+      <section className="hero">
+      <div className="hero-left">
+            <span className="hero-tag">
+               Blockchain • Secure • Decentralized
+           </span>
+      <h1>
+      Blockchain Academic
+      <br />
+      Certificate Verification
+    </h1>
+    <p>
+      CertChain is a blockchain-powered platform
+      that enables educational institutions to
+      issue, manage and verify academic
+      certificates securely using Ethereum smart
+      contracts.
+    </p>
+    <div className="hero-buttons">
+      <button className="primary-btn">
+        Verify Certificate
+      </button>
+      <button className="secondary-btn">
+        Institute Portal
+      </button>
+    </div>
+    <div className="hero-highlights">
+      <div>
+        🔒
+        <span>Tamper Proof</span>
+      </div>
+      <div>
+        ⚡
+        <span>Instant Verification</span>
+      </div>
+        <div>
+        ⛓️
+        <span>Blockchain Secured</span>
           </div>
-          <div className="hero-right">
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/3135/3135768.png"
-              alt="certificate"
-            />
-          </div>
-        </section>
+        </div>
+       </div>
+      <div className="hero-right">
+       <img
+           src="https://cdn-icons-png.flaticon.com/512/3135/3135768.png"
+           alt="Certificate"
+        />
+       </div>
+       </section>
         <section className="features">
           <h2>
             Why Choose CertChain?
