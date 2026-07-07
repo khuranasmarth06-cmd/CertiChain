70fdc95a2af5818f88f59f366b0bbddc10165354
smarth khurana <khuranasmarth06@gmail.com>
2026-07-08 00:26:25 +0530

    initiated wegmi

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
 import App from "./App";
 import { ThemeProvider } from "./context/ThemeContext";
+import { config } from "./config/wagmi";
 import "./styles/App.css";
 import "./styles/theme.css";
+const queryClient = new QueryClient();
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <WagmiProvider config={config}>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </WagmiProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
